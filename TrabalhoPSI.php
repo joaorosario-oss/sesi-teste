5ed2cca26e3f9ec8f1d2f37d656a58f3822f81e5
joaorosario-oss <joao.rosario@edu.senai.br>
2026-04-23 07:48:55 -0300

    UpTrabalhoPSI.php

--- TrabalhoPSI.php
+++ TrabalhoPSI.php
@@ -44,10 +44,42 @@ if ($escolha==3){
 echo "encerrando programa";
 
 }
+if ($escolha==2){
+echo "termos gerais psi";
+$topico = readline ("qual tópico você deseja ver?\n")
+echo "------------------------\n";
+echo "▪ 1-Objetivos da PSI\n";
+echo "▪ 2-Escopo da política\n";
+echo "▪ 3-Responsabilidades\n";
+echo "▪ 4-Boas práticas de segurança\n";
+echo "▪ 5-Consequências do descumprimento\n";
+
+switch ($topico) {
+
+case 1
+echo "objetivo da psi";
+break
+
+case 2
+echo "escopo da política";
+break
+} 
+
+}
+if ($escolha==2){
+echo "termos Gerais psi";
 
 
+}
+if ($escolha==3){
+echo "encerrando programa";
+
+}
 
 
+if (opcao==3)  {
+echo"encerrando sistema";
+}
 
 
 
